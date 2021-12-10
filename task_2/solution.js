f2f50d1d1faed580f5d9b924f6fb961ff1235629
Kristina Aleshenko <kristinka21-91@mail.ru>
2021-12-10 15:28:58 +0300

    task 2 done

--- task_2/solution.js
+++ task_2/solution.js
@@ -15,9 +15,20 @@ function calcShipping(sum, min, shipping) {
 
     // если productsSum больше 0 и меньше freeShippingMinSum,
     // то shippingSum присвоить значение shippingPrice
+    let shippingSum;
+    if (productsSum === 0){
+        shippingSum = 0;
+    }
+    if (productsSum >= freeShippingMinSum){
+        shippingSum = 0;
+    }
+    if (productsSum > 0 && productsSum < freeShippingMinSum){
+        shippingSum = shippingPrice;
+    }
+    console.log (shippingSum);
 
     // Конец решения задания №2.1.
-
+    
     return shippingSum;
 }
 
@@ -33,7 +44,13 @@ function calcDiscount(sum, min, discount) {
     // если productsSum больше или равно discountMinSum,
     // то присвойте discountSum значение discountPart процентов от productsSum,
     // иначе присвойте discountSum значение 0
-
+    let discountSum;
+    if (productsSum >= discountMinSum){
+        discountSum = discountPart * productsSum / 100;
+    } else {
+        discountSum = 0;
+    }
+    console.log (discountSum);
     // Конец решения задания №2.2.
 
     return discountSum;
@@ -49,14 +66,21 @@ function calcInvoice({sum, discountMinSum, discountPart, shippingFreeMinSum, shi
 
     // присвойте totalSum значение productsSum
     // уменьшите totalSum на discountSum
-
+    let totalSum;
+    totalSum = productsSum;
+    totalSum = totalSum - discountSum;
+    
     let shippingSum = calcShipping(totalSum, shippingFreeMinSum, shippingPrice); // не изменяйте эту строку!!!
-
+    totalSum = totalSum + shippingSum;
     // прибавьте к totalSum значение shippingSum
 
     // создайте переменную freeShipping
     // запишите без использования if или любых других условий:
     // если shippingSum равно нулю, то freeShipping должна быть равна true, иначе freeShipping должна быть равна false
+    
+
+    let freeShipping;
+    freeShipping = !shippingSum;
 
     // Конец решения задачи №2.3.
 
